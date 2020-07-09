b6d065ead81d3cf1ac90e53056a3fdd2524b26ae
chow <chow100@aliyun.com>
2020-07-09 18:17:39 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/chow/springcloud/CloudProviderPayment8001.java
@@ -0,0 +1,13 @@
+package com.chow.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProviderPayment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001.class);
+    }
+
+}
