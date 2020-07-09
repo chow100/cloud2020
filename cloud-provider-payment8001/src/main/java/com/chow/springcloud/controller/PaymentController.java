2c2892caee3353cda53f8461026e28027480bd5f
chow100 <chow100@aliyun.com>
2020-07-10 02:36:39 +0800

    增加实现

--- cloud-provider-payment8001/src/main/java/com/chow/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/chow/springcloud/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.chow.springcloud.controller;
 
 import com.chow.springcloud.entities.CommonResult;
 import com.chow.springcloud.entities.Payment;
+import com.chow.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ cloud-provider-payment8001/src/main/java/com/chow/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.chow.springcloud.service.impl;
+
+import com.chow.springcloud.dao.PaymentDao;
+import com.chow.springcloud.entities.Payment;
+import com.chow.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
+ 
+ 
