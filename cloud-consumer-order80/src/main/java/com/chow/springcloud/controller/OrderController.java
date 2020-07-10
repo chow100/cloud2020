d00292e7626060133a8b47f4f51082947ddca6ed
chow <chow100@aliyun.com>
2020-07-10 11:20:23 +0800

    fix sql $ to #. Add @RequestBody.

--- cloud-consumer-order80/src/main/java/com/chow/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/chow/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.chow.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,12 +21,12 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment) {
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);  //写操作
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/get/" + id, null, CommonResult.class);
     }
 }
