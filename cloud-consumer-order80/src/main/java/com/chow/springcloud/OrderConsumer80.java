8dcb30f12b13f04abaaaa50b0a2ee7be426b84cd
chow100 <chow100@aliyun.com>
2020-07-13 17:41:50 +0800

    add eureka service

--- cloud-consumer-order80/src/main/java/com/chow/springcloud/OrderConsumer80.java
+++ cloud-consumer-order80/src/main/java/com/chow/springcloud/OrderConsumer80.java
@@ -2,12 +2,14 @@ package com.chow.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class CloudProviderPayment8001 {
+@EnableEurekaClient
+public class OrderConsumer80 {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudProviderPayment8001.class);
+        SpringApplication.run(OrderConsumer80.class);
     }
 
 }
--- cloud-provider-payment8001/src/main/java/com/chow/springcloud/PaymentProvider8001.java
+++ cloud-provider-payment8001/src/main/java/com/chow/springcloud/PaymentProvider8001.java
@@ -2,12 +2,14 @@ package com.chow.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class CloudConsumerOrder80 {
+@EnableEurekaClient
+public class PaymentProvider8001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudConsumerOrder80.class);
+        SpringApplication.run(PaymentProvider8001.class);
     }
 
 }
